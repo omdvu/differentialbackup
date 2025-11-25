0f37bf0fb0f330dd1650563c91afd8aef29c08ab
Om Prajapati <omprajapatidv@gmail.com>
2025-11-25 20:42:15 +0530

    2.1

--- mirrorbackup.py
+++ mirrorbackup.py
@@ -1,7 +1,6 @@
 import os
 import shutil
 import json
-import differential
 
 def mirrorbackup(sourcemnt,destmnt,mirrorlogdir):
     if os.path.exists(destmnt):
@@ -12,9 +11,4 @@ def mirrorbackup(sourcemnt,destmnt,mirrorlogdir):
             return
     
     shutil.copytree(sourcemnt,destmnt,ignore=shutil.ignore_patterns('serverlog.txt','mountlog.txt'),copy_function=shutil.copy2)
-
-    mirroredfiles = differential.explore(sourcemnt)
-    with open(mirrorlogdir,'w') as f:
-        json.dump(mirroredfiles,f)
-
     print("Mirrored!")
