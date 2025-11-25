0e32abf199ecbeaf66f66140a8292a460204d1d6
Om Prajapati <omprajapatidv@gmail.com>
2025-11-25 21:12:07 +0530

    2.1

--- mirrorbackup.py
+++ mirrorbackup.py
@@ -2,13 +2,24 @@ import os
 import shutil
 import json
 
-def mirrorbackup(sourcemnt,destmnt):
-    if os.path.exists(destmnt):
-        try: 
-            shutil.rmtree(destmnt)
+def mirrorbackup(sourcemnt, destmnt):
+    for item in os.listdir(destmnt):
+        item_path = os.path.join(destmnt, item)
+        try:
+            if os.path.isfile(item_path) or os.path.islink(item_path):
+                os.unlink(item_path)
+            else:
+                shutil.rmtree(item_path)
         except Exception as e:
-            print(f"Error removing old mirror: {e}")
+            print(f"Error removing {item_path}: {e}")
             return
-    
-    shutil.copytree(sourcemnt,destmnt,ignore=shutil.ignore_patterns('serverlog.txt','mountlog.txt'),copy_function=shutil.copy2)
+
+    shutil.copytree(
+        sourcemnt,
+        destmnt,
+        dirs_exist_ok=True,
+        ignore=shutil.ignore_patterns("serverlog.txt", "mountlog.txt","mnt"),
+        copy_function=shutil.copy2
+    )
+
     print("Mirrored!")
